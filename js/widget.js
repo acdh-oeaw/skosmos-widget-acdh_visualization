920f3b4435568a87424b85189379d7db7ea1da5d
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2019-10-11 12:28:29 +0000

    get conceptinfo via rest style adjustments

--- js/widget.js
+++ js/widget.js
@@ -1,27 +1,60 @@
 var ACDH_VISUALIZATION = ACDH_VISUALIZATION || {};
 
 ACDH_VISUALIZATION = {
-
-	narrower_concepts: [],
+	//json-ld data for this concept (includes narrower and broader concept)
+	jsonldgraph: {},
+	//filtered concept data from json-ld data for this concept
+	currentconcept: {},
+	//properties that should be visualized
+	visprops: ["narrower","broader","topConceptOf","skos:exactMatch","skos:narrowMatch"],
+	// properties, for which labels need to be plugged from graph
+	enrichprops: ["narrower", "broader", "skos:topConceptOf"],
 	graph_data: {
 		nodes: [],
 		edges: [],
 		types: {
 			nodes: [{
-					id: 't1',
-					label: 'Category 1',
-					color: '#5CC0C4'
-				},
-				{
-					id: 't2',
-					label: 'Category 2',
-					color: '#88D8DF'
-				}
+				id: 'concept',
+				label: 'concept',
+				color: '#5CC0C4'
+			},
+			{
+				id: 'narrower',
+				label: 'narrower',
+				color: '#88D8DF'
+			},
+			{
+				id:'broader',
+				label:'broader',
+				color:"#2C8891"
+			},
+			{
+				id:'skos:narrowMatch',
+				label:'skos:narrowMatch',
+				color:"#ffcc33"
+			},
+			{
+				id:'skos:exactMatch',
+				label:'skos:narrowMatch',
+				color:"#0047bb"
+			}
 			],
 			edges: [{
-				id: 'r1',
-				label: 'Relation 1',
+				id: 'broader',
+				label: 'broader',
 				color: '#990066'
+			}, {
+				id: 'broaderTransitive',
+				label: 'broaderTransitive',
+				color: '#990066'
+			}, {
+				id: 'narrower',
+				label: 'narrower',
+				color: '#00748f'
+			}, {
+				id: 'related',
+				label: 'related',
+				color: '#FF9900'
 			}]
 		},
 		replace: true
@@ -29,20 +62,68 @@ ACDH_VISUALIZATION = {
 
 	/* accessing graph data from page only possible starting from skosmos v.2 graph as JSON-LD 
 	https://github.com/NatLibFi/Skosmos/wiki/Embedded-JSON-LD#requirements-and-definition */
-	getNarrowerConcepts: function () {
-		ACDH_VISUALIZATION.narrower_concepts.length = 0;
-		console.log(ACDH_VISUALIZATION.narrower_concepts);
-		var nclist = $("div.row:contains('NARROWER CONCEPTS')").find(".property-value-wrapper").find("ul li a");
-		nclist.each(function (i, el) {
-			var nConcept = {};
-			nConcept = {
-				"label": $.trim($(el).text()),
-				"uri": $(el).attr("href")
-			}
-			ACDH_VISUALIZATION.narrower_concepts.push(nConcept);
+	
+	createNodeTypes: function() {
+		ACDH_VISUALIZATION.visprops.forEach(function(visprop) {
+			ACDH_VISUALIZATION
 		})
 	},
-	createNode(label,type) {
+	
+	getJSONLD: async function (vocab, uri, prop) {
+		var url = "rest/v1/" + vocab + "/" + prop + "?uri=" + uri;
+		var res = await fetch(url);
+		var data = await res.json();
+		return data;
+	},
+	getConceptData: async function (vocab, uri) {
+
+		var res = await fetch("rest/v1/" + vocab + "/data?uri=" + uri + "&format=application/ld%2Bjson");
+		var resdata = await res.json();
+
+		ACDH_VISUALIZATION.jsonldgraph = resdata.graph;
+		ACDH_VISUALIZATION.currentconcept = ACDH_VISUALIZATION.findConceptInGraph(uri);
+
+	},
+
+	enrichConceptData: function () {
+
+		ACDH_VISUALIZATION.enrichprops.forEach(function (enrichprop) {
+
+			if (ACDH_VISUALIZATION.currentconcept[enrichprop] instanceof Array) {
+				ACDH_VISUALIZATION.currentconcept[enrichprop].forEach(function (concept) {
+
+					var conceptforprop = ACDH_VISUALIZATION.findConceptInGraph(concept.uri);
+					concept.label = ACDH_VISUALIZATION.getPrefLabelForLang(conceptforprop.prefLabel);
+				})
+
+			} else if (ACDH_VISUALIZATION.currentconcept[enrichprop] instanceof Object) {
+				var conceptforprop = ACDH_VISUALIZATION.findConceptInGraph(ACDH_VISUALIZATION.currentconcept[enrichprop].uri);
+
+				if (conceptforprop.prefLabel && conceptforprop.prefLabel.length > 0) {
+
+					ACDH_VISUALIZATION.currentconcept[enrichprop].label = ACDH_VISUALIZATION.getPrefLabelForLang(conceptforprop.prefLabel);
+
+				}
+				else if (conceptforprop.label) {
+					ACDH_VISUALIZATION.currentconcept[enrichprop].label = conceptforprop.label;
+				}
+			}
+		});
+
+	},
+	findConceptInGraph: function (concepturi) {
+		return ACDH_VISUALIZATION.jsonldgraph.filter(function (cobj) {
+			return cobj.uri == concepturi;
+		})[0];
+
+	},
+	getPrefLabelForLang: function (labels) {
+		return labels.filter(function (label) {
+			return label.lang == content_lang;
+		})[0].value;
+	},
+
+	createNode(label, type) {
 		var id = "n" + ACDH_VISUALIZATION.graph_data.nodes.length;
 		return {
 			"id": id,
@@ -50,39 +131,65 @@ ACDH_VISUALIZATION = {
 			"type": type
 		};
 	},
-	createEdge(label, src, target) {
+	createEdge(label, src, target, type) {
 		var id = "e" + ACDH_VISUALIZATION.graph_data.edges.length;
 		return {
 			"id": id,
 			"label": label,
 			"source": src,
 			"target": target,
-			"type": "r1"
+			"type": type
 		};
 	},
 	emptyVisData: function () {
 		ACDH_VISUALIZATION.graph_data.nodes.length = 0;
 		ACDH_VISUALIZATION.graph_data.edges.length = 0;
 	},
-	createVisData: function (data) {
-		var mainNode = ACDH_VISUALIZATION.createNode(data.prefLabels[0].label,"t1");
-		ACDH_VISUALIZATION.graph_data.nodes.push(mainNode);
-		ACDH_VISUALIZATION.narrower_concepts.forEach(function (nc, i) {
-			ACDH_VISUALIZATION.graph_data.nodes.push(ACDH_VISUALIZATION.createNode(nc.label,"t2"))
-			ACDH_VISUALIZATION.graph_data.edges.push(ACDH_VISUALIZATION.createEdge("skos:narrower", ACDH_VISUALIZATION.graph_data.nodes[0].id, ACDH_VISUALIZATION.graph_data.nodes[i + 1].id))
-		})
-		var graph = ACDH_VISUALIZATION.graph_data;
+	createVisData: function () {
 
+		var mainNode = ACDH_VISUALIZATION.createNode(ACDH_VISUALIZATION.currentConcept.prefLabels[0].label, "concept");
+		ACDH_VISUALIZATION.graph_data.nodes.push(mainNode);
+		ACDH_VISUALIZATION.visprops.forEach(function(visprop) {
+			if (ACDH_VISUALIZATION.currentconcept[visprop]) {
+					if (ACDH_VISUALIZATION.currentconcept[visprop] instanceof Array) {
+						ACDH_VISUALIZATION.currentconcept[visprop].forEach(function(obj) {
+							var newNode = {};
+							if (obj.label) {
+								newNode = ACDH_VISUALIZATION.createNode(obj.label, visprop);
+							} else {
+								newNode = ACDH_VISUALIZATION.createNode(obj.uri, visprop);
+							}
+							ACDH_VISUALIZATION.graph_data.nodes.push(newNode);
+							ACDH_VISUALIZATION.graph_data.edges.push(ACDH_VISUALIZATION.createEdge(visprop, ACDH_VISUALIZATION.graph_data.nodes[0].id, newNode.id, visprop));
+						})
+					}
+					else if (ACDH_VISUALIZATION.currentconcept[visprop] instanceof Object) {
+						var obj = ACDH_VISUALIZATION.currentconcept[visprop];
+						var newNode = {};
+						if (obj.label) {
+							newNode = ACDH_VISUALIZATION.createNode(obj.label, visprop);
+						} else {
+							newNode = ACDH_VISUALIZATION.createNode(obj.uri, visprop);
+						}
+						ACDH_VISUALIZATION.graph_data.nodes.push(newNode);
+						ACDH_VISUALIZATION.graph_data.edges.push(ACDH_VISUALIZATION.createEdge(visprop, ACDH_VISUALIZATION.graph_data.nodes[0].id, newNode.id, visprop));
+					}
+			}
+			
+		});
+		var graph = ACDH_VISUALIZATION.graph_data;
+		console.log(ACDH_VISUALIZATION.graph_data);
 		ReactDOM.render(
 			React.createElement(NetworkVisualization.SelectionControls, {
 				graph
 			}),
-			document.getElementById('acdh_visualization')
+			$('#acdh_visualization .widget-body')[0]
 		)
 
 	},
-	render: function (object) {
-		$('.concept-info').after(Handlebars.compile($('#acdh_visualization-template').html())(object));
+	render: function (data) {
+		$('.concept-info').after(Handlebars.compile($('#acdh_visualization-template').html())(data));
+		ACDH_VISUALIZATION.currentConcept = data;
 	}
 };
 
@@ -92,9 +199,11 @@ $(function () {
 		if (data.prefLabels) {
 			ACDH_VISUALIZATION.emptyVisData();
 			ACDH_VISUALIZATION.render(data);
-			ACDH_VISUALIZATION.getNarrowerConcepts();
-			ACDH_VISUALIZATION.createVisData(data);
-		}
+			ACDH_VISUALIZATION.getConceptData(vocab, data.uri).then(function () {
+				ACDH_VISUALIZATION.enrichConceptData();
+				ACDH_VISUALIZATION.createVisData();
+			});
+		};
 	};
 	$('#tree').on("select_node.jstree", function (e, data) {
 		window.acdh_visualization(data);
